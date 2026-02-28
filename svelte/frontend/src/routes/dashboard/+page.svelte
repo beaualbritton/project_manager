12f8b58f7846c8b4e72c571ff4d1179280babf27
beau albritton <albrittoncb@gmail.com>
2026-02-28 11:46:49 -0500

    feat: navbar

--- svelte/frontend/src/routes/dashboard/+page.svelte
+++ svelte/frontend/src/routes/dashboard/+page.svelte
@@ -1,15 +1,3 @@
-<script lang="ts">
-import { logout } from "$lib/api/login";
-import WrapperButton from "$lib/components/wrapper_button.svelte";
-async function logoutSubmit()
-{
-  let response = await logout();
-}
-</script>
-
 <main>
-  <div>
-  <WrapperButton title="logout" toggle={logout}/>
-  </div>
-
+  dashboard
 </main>
