6093016c204991b5bd09758845047f08d8e65f33
beau albritton <albrittoncb@gmail.com>
2026-02-28 11:46:49 -0500

    feat: navbar

---
+++ svelte/frontend/src/lib/components/nav.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+import { logout } from "$lib/api/login";
+import WrapperButton from "$lib/components/wrapper_button.svelte";
+async function logoutSubmit()
+{
+  let response = await logout();
+}
+</script>
+
+<div>
+  <WrapperButton title="logout" toggle={logout}/>
+</div>
+
+
